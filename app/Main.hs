35e0f46cb8dbdb35653783a1678a9119ba75b19a
Fernando Ayats <ayatsfer@gmail.com>
2025-05-03 10:15:25 +0200

    Use chans

--- app/Main.hs
+++ app/Main.hs
@@ -1,63 +1,96 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
-import Control.Concurrent (MVar, forkIO, newMVar, readMVar, swapMVar, threadDelay)
+import Control.Concurrent (forkIO, newMVar, putMVar, readMVar, takeMVar, threadDelay)
+import Control.Concurrent.Chan
 import Control.Exception (bracket_)
+import Control.Monad (forever, void)
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe, listToMaybe)
 import Data.String
+import qualified Data.Text as T
+import Data.Text.Encoding (decodeUtf8)
 import Network.HTTP.Types
 import Network.Wai
-import Network.Wai.Handler.Warp (defaultSettings, setHost, runSettings, setPort)
+import Network.Wai.Handler.Warp (Settings, defaultSettings, runSettings, setHost, setPort)
 import System.Environment (lookupEnv)
-import Data.Maybe (fromMaybe)
 
-main :: IO ()
-main = do
+type State = Map.Map T.Text Integer
+
+settings :: IO Settings
+settings = do
     p <- lookupEnv "PORT"
     let port = maybe 8080 read p
     putStrLn $ "Running on port " <> show port
-
     h <- lookupEnv "HOSTNAME"
     let host = fromMaybe "0.0.0.0" h
     putStrLn $ "Running on host " <> show host
+    return $ (setPort port . setHost (fromString host)) defaultSettings
 
-    let settings = (setPort port . setHost (fromString host)) defaultSettings
+main :: IO ()
+main = do
+    s <- settings
+    channels <- stateHandler
 
-
-    counter <- newMVar 0
     bracket_
         (putStrLn "Starting server")
         (putStrLn "Goodbye")
-        (runSettings settings (app counter))
+        (runSettings s (app channels))
 
-app :: MVar Integer -> Application
-app counter req respond =
-    bracket_
-        (print req)
-        -- (putStrLn "Cleaning up")
-        (return ())
-        ( do
-            bump counter
-            value <- readMVar counter
-            respond $ responseLBS status200 [] (fromString ("Active Users: " <> show value))
-        )
+app :: Channels -> Application
+app (tx, rx) req respond = do
+    print req
+    let headers = requestHeaders req
+    let x_forwarded_for = T.words . decodeUtf8 <$> lookup "X-Forwarded-For" headers
+    let ip_source = T.replace "," "" <$> (listToMaybe =<< x_forwarded_for)
 
-seconds :: Integer -> Integer
+    case ip_source of
+        Just ip -> do
+            writeChan tx (PutIP ip)
+            void $ forkIO $ do
+                threadDelay (seconds 10)
+                writeChan tx (DeleteIP ip)
+        Nothing -> putStrLn "X-Forwarded-For unset"
+
+    writeChan tx TotalIP
+    total <- readChan rx
+
+    respond $ responseLBS status200 [] (fromString ("Active Users: " <> show total))
+
+seconds :: Int -> Int
 seconds = (* 1_000_000)
 
-minutes :: Integer -> Integer
+minutes :: Int -> Int
 minutes = (* 60) . seconds
 
-bump :: MVar Integer -> IO ()
-bump counter = do
-    oldValue <- readMVar counter
-    let newValue = oldValue + 1
-    _ <- swapMVar counter newValue
-    print $ "Active Users: " <> show newValue
+type Channels = (Chan StateMsg, Chan Int)
 
-    _ <- forkIO $ do
-        threadDelay 5_000_000
-        oldValue' <- readMVar counter
-        let newValue' = oldValue' - 1
-        _ <- swapMVar counter newValue'
-        print $ "Rollback: Active Users: " <> show newValue'
+data StateMsg = PutIP T.Text | DeleteIP T.Text | TotalIP
 
-    return ()
+stateHandler :: IO (Chan StateMsg, Chan Int)
+stateHandler = do
+    rx :: Chan StateMsg <- newChan
+    tx :: Chan Int <- newChan
+
+    ipMap <- newMVar Map.empty
+
+    _ <- forkIO $ forever $ do
+        next <- readChan rx
+        case next of
+            PutIP ip -> do
+                old <- takeMVar ipMap
+                let new = Map.insert ip () old
+                putMVar ipMap new
+            DeleteIP ip -> do
+                old <- takeMVar ipMap
+                let new = Map.delete ip old
+                putMVar ipMap new
+            TotalIP -> do
+                old <- readMVar ipMap
+                let total = Map.size old
+                writeChan tx total
+
+        readMVar ipMap >>= print
+
+    return (rx, tx)
